3dcb0e93df6a5eec219beea103c6fe1f9b198191
XT60 <89086129+XT60@users.noreply.github.com>
2022-10-10 18:08:23 +0200

    work done on labs

---
+++ src/main/java/agh/ics/oop/MoveDirection.java
@@ -0,0 +1,12 @@
+package agh.ics.oop;
+
+public enum MoveDirection {
+    FORWARD, BACKWARD, RIGHT, LEFT;
+
+    @Override
+//    public String toString() {
+//        if (this){
+//            case F
+//        }
+//    }
+}
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -4,10 +4,15 @@ import static java.lang.System.out;
 
 public class World {
     public static void main(String[] args) {
-        out.println("system wystartował");
-        Direction[] arr = convertToDirection(args);
-        run(arr);
-        out.println("system zakończył działanie");
+//        out.println("system wystartował");
+//        Direction[] arr = convertToDirection(args);
+//        run(arr);
+//        out.println("system zakończył działanie");
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
     static void run(Direction[] arr){
         int len = arr.length;
